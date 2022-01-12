8d9953f73647583d2790b9bc166fc5f9d8c23cac
Ephraim Tlhako <ephraim.tlhako@dadevs.co.za>
2022-01-12 14:42:15 +0200

    fixed conflicts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,29 +5,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ListMovieComponent } from './components/list-movie/list-movie.component';
-
-=======
 import { SearchComponent } from './components/search/search.component';
->>>>>>> 2460e423a1142ef5bed24f8d43a99de242d65576
-=======
 import { AddComponent } from './components/add/add.component';
->>>>>>> b1870679082b3199c356b95ed260dd9aacde6a61
-
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ListMovieComponent
-=======
-    SearchComponent
->>>>>>> 2460e423a1142ef5bed24f8d43a99de242d65576
-=======
-    AddComponent
->>>>>>> b1870679082b3199c356b95ed260dd9aacde6a61
+    ListMovieComponent,
+    SearchComponent,
+    AddComponent,
   ],
   imports: [
     BrowserModule,
