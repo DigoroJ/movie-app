f1a961d66a11251f475d32cd3dab1a50d5532153
Simon Ntini <simon.ntini@dadevs.co.za>
2022-01-12 16:08:33 +0200

    simon.ntini

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/add/add.component';
+import { ListMovieComponent } from './components/list-movie/list-movie.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'movies', pathMatch: 'full'},
+  {path: 'movies',component: ListMovieComponent},
+  {path: 'add', component: AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
