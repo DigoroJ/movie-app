b49cd2eba84e5fd5d201d0fe3f64f83238e58913
DA_learner <da_learner@DA-learners-MacBook-Air.local>
2022-01-12 12:12:22 +0200

    display movies

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListMovieComponent } from './components/list-movie/list-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListMovieComponent
   ],
   imports: [
     BrowserModule,
