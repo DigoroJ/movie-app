8e50a7036b7b60c2ed6c0c5c5f678609a2aeb371
Joseph Digoro <joseph.digoro@dadevs.co.za>
2022-01-13 11:17:03 +0200

    Adding button working

--- front-end/src/app/components/add/add.component.ts
+++ front-end/src/app/components/add/add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  movieForm = this.fb.group({
+    title: [''],
+    length: [''],
+    description: [''],
+    genre: [''],
+    year: [''],
+    rating: ['']
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _movieService: MovieService ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    //passing the values in movie
+  const movie: Movie = {
+    title: this.movieForm.value.title, 
+    description: this.movieForm.value.description,
+    genre: this.movieForm.value.genre,
+    rate: this.movieForm.value.rating,
+    year: this.movieForm.value.year,
+    length: this.movieForm.value.length
+  }
+    this._movieService.create(movie)
+
+    .subscribe( data => console.log('working', data)
+    );
+   
+    
+  }
 }
