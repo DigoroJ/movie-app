786f2915db297ea72e0b7e532c912ad2d256016c
DA_learner <da_learner@DA-learners-MacBook-Air.local>
2022-01-13 10:59:49 +0200

    with edit

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './components/add/add.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
 import { ListMovieComponent } from './components/list-movie/list-movie.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'movies', pathMatch: 'full'},
   {path: 'movies',component: ListMovieComponent},
-  {path: 'add', component: AddComponent}
+  {path: 'add', component: AddComponent},
+  { path: "editmovie/:id", component: EditMovieComponent}
 ];
 
 @NgModule({
--- front-end/src/app/components/list-movie/list-movie.component.ts
+++ front-end/src/app/components/list-movie/list-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie.model';
 import { MovieService } from 'src/app/services/movie.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-list-movie',
@@ -15,14 +16,26 @@ export class ListMovieComponent implements OnInit {
   constructor(private ms: MovieService) { }
 
   ngOnInit(): void {
-   this.ms.getAll().subscribe(data => {
-     this.movies = data;
-     console.log(this.movies)
-   })
+   this.getMovies();
+  }
+
+  getMovies(){
+    this.ms.getAll().subscribe(data => { this.movies = data})
   }
 
   deleteMovie(id: any){
-    console.log(id);
+   this.ms.delete(id).subscribe((data) => {
+
+      //removing delete movie from movie array 
+      this.movies = this.movies.filter((movie) => {
+        return movie.id !== id;
+      });
+
+      Swal.fire({
+        title: "You have deleted successfully.",
+      })
+    })
+   
   }
 
 }
