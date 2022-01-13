fab72f2473acdfe336da3942fab70f045f1bb921
Joseph Digoro <joseph.digoro@dadevs.co.za>
2022-01-13 16:54:12 +0200

    Search

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -8,16 +9,27 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class SearchComponent implements OnInit {
   
-   data='';
-
-  constructor(private ms: MovieService) { }
+   
+   searchForm = this.fb.group({
+    option: [''],
+    searchValue:['']
+  });
+  constructor(private fb: FormBuilder, private ms: MovieService) { }
+   
 
   ngOnInit(): void {
   }
 
   search():void{
-    this.ms.search(this.data).subscribe({
+
+    const searchData:any = {
+        value: this.searchForm.value.searchValue,
+        option:this.searchForm.value.option
+    };
+
+    this.ms.search(searchData).subscribe({
       next:(data)=>{
+         console.log(data);
          
       },
       error:(e)=> console.error(e)
