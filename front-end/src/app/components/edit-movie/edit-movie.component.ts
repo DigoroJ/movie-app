7f2f7b6bb18ee587f341292c22508be9047519da
DA_learner <da_learner@DA-learners-MacBook-Air.local>
2022-01-14 08:34:49 +0200

    edit

--- front-end/src/app/components/edit-movie/edit-movie.component.ts
+++ front-end/src/app/components/edit-movie/edit-movie.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-movie',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMovieComponent implements OnInit {
 
-  constructor() { }
+  movie: Movie = {};
+  id: any = 0;
+
+  editMovieForm = this.fb.group({
+    title: [''],
+    length: [''],
+    description: [''],
+    genre: [''],
+    year: [''],
+    rating: ['']
+  });
+  
+  constructor(private route: ActivatedRoute, private ms: MovieService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
+    this.populateForm();
+  }
+
+  onSubmit(){
+    console.log("on submit")
+  }
+
+  populateForm(){
+    this.ms.get(this.id).
+    subscribe(data => {
+      this.editMovieForm.patchValue({
+        title: data.title,
+        length: data.length,
+        description: data.description,
+        genre: data.genre,
+        year: data.year,
+        rating: data.rate
+      })
+    });
+  }
+
+  onUpdate(){
+    console.log(this.editMovieForm.value);
+
+    //populate the values
+    const movie: Movie = {
+      title: this.editMovieForm.value.title, 
+      description: this.editMovieForm.value.description,
+      genre: this.editMovieForm.value.genre,
+      rate: this.editMovieForm.value.rating,
+      year: this.editMovieForm.value.year,
+      length: this.editMovieForm.value.length
+    }
+
+  this.ms.update(this.id, movie).subscribe((data) => {Swal.fire({title: "You have updated successfully."})})
   }
 
 }
--- front-end/src/app/components/list-movie/list-movie.component.ts
+++ front-end/src/app/components/list-movie/list-movie.component.ts
@@ -12,7 +12,6 @@ export class ListMovieComponent implements OnInit {
 
   movies: Movie[] = []
  
-
   constructor(private ms: MovieService) { }
 
   ngOnInit(): void {
